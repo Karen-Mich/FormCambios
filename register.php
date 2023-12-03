ebcff25b5fa0a3221b890ac452b87b42633b49e3
Karen-Mich <kguatumillo3914@uta.edu.ec>
2023-12-03 00:18:29 -0500

    Consulta de insercion de usuarios

---
+++ register.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    <form class = "formulario" method="post"> 
+        <div class = "contenedor">
+            <h1>Crear Cuenta</h1>
+    <div class = "input-contenedor">
+        <i class='bx bxs-user-circle'></i>
+<input type = "text" name="nombre1" placeholder = "Primer nombre">
+</div>
+
+<div class = "input-contenedor">
+    <i class='bx bx-user-circle' ></i>
+<input type = "text" name="nombre2" placeholder = "Segundo Nombre">
+</div>
+
+<div class = "input-contenedor">
+    <i class='bx bxs-user-rectangle' ></i>
+<input type = "text" name="apellido1" placeholder = "Primer Apellido">
+</div>
+
+<div class = "input-contenedor">
+    <i class='bx bx-user-pin' ></i>
+<input type = "text" name="apellido2" placeholder = "Segundo Apellido">
+</div>
+
+<div class = "input-contenedor">
+    <i class='bx bxs-envelope'></i>
+<input type = "text" name="correo" placeholder = "Correo electrónico">
+</div>
+
+<div class = "input-contenedor">
+    <i class='bx bxs-map' ></i>
+<input type = "text" name="direccion" placeholder = "Dirección">
+</div>
+
+<div class = "input-contenedor">
+    <i class='bx bxs-phone-incoming' ></i>
+<input type = "text" name="telefono" placeholder = "Teléfono">
+</div>
+
+<div class = "input-contenedor">
+    <i class='bx bxs-key' ></i>
+<input type = "password" name="contrasena" placeholder = "Contraseña">
+</div>
+
+<input type="submit" value = "Registrar" name = 'registrar' class = "button">
+<p>Al registrarte, aceptas nuestras Condiciones de uso y Politica de privacidad.</p>
+<p>¿Ya tienes cuenta? <a class="link" href="?">Iniciar Sesion</a></p> 
+    </div>
+    </form>
+    <?php
+    include("registrar.php");
+    ?>
+</body>
+</html>
---
+++ registrar.php
@@ -0,0 +1,42 @@
+<?php
+include("conexion.php");
+
+if(isset($_POST['registrar'])){
+    if(strlen($_POST['nombre1']) >= 1 &&
+    strlen($_POST['nombre2']) >=1 &&
+    strlen($_POST['apellido1']) >=1 &&
+    strlen($_POST['apellido2']) >=1 &&
+    strlen($_POST['correo']) >=1 &&
+    strlen($_POST['direccion']) >=1 &&
+    strlen($_POST['telefono']) >=1 &&
+    strlen($_POST['contrasena']) >=1 ){
+        $nombre1 = trim($_POST['nombre1']);
+        $nombre2=trim($_POST['nombre2']);
+        $apellido1=trim($_POST['apellido1']);
+        $apellido2=trim($_POST['apellido2']);
+        $correo=trim($_POST['correo']);
+        $telefono=trim($_POST['telefono']);
+        $direccion=trim($_POST['direccion']);
+        $contrasena=trim($_POST['contrasena']);
+        $consulta = "INSERT INTO usuario (ID_USU,NOM_USU, NOM2_USU, APE_USU, APE2_USU, COR_USU, DIR_USU, TEL_USU, CON_USU,EST_USU)
+        VALUES ('2','$nombre1', '$nombre2', '$apellido1', '$apellido2', '$correo', '$direccion', '$telefono', '$contrasena','1');";
+        $resultado = mysqli_query($conex, $consulta);
+       
+       /* if ($resultado){
+            ?>
+            <h3 class="ok">Inscrito correctamente </h3>
+            <?php
+        } else {
+            ?>
+            <h3 class="bad">ha ocurrido un error</h3>
+            <?php
+        } else {
+            ?>
+            <h3 class="bad">Complete los campos</h3>
+            <?php
+
+    }*/
+}
+}
+
+?>
