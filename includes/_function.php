bf2df3c55db782866083bf0e56996d792b1248fe
Hamilton Jumbo <hamil.jumbo@gmail.com>
2023-12-03 12:56:37 -0500

    Docuemntacion de la funcionalidad del LOGIN

--- includes/_function.php
+++ includes/_function.php
@@ -1,13 +1,28 @@
 <?php
-    $nombre=$_POST['nombre'];
-    $password=$_POST['password'];
+    $nombre = $_POST['nombre'];
+    $password = $_POST['password'];
     session_start();
-    $_SESSION['nombre']=$nombre;
-    $conexion=mysqli_connect("localhost","root","admin","formulario_de_cambios");
-    $consulta= "SELECT * FROM usuario WHERE NOM_USU='$nombre' AND CON_USU='$password'";
-    $resultado=mysqli_query($conexion, $consulta);
-    $filas=mysqli_fetch_array($resultado);
-    header('Location: ../views/hamilton.php');
+    $_SESSION['nombre'] = $nombre;
+    $conexion = mysqli_connect("localhost", "root", "admin", "formulario_de_cambios");
+    
+    $consulta = "SELECT * FROM usuario WHERE NOM_USU='$nombre' AND CON_USU='$password'";
+    $resultado = mysqli_query($conexion, $consulta);
+    
+    // Verificar si hay filas en el resultado (usuario válido)
+    if (mysqli_num_rows($resultado) > 0) {
+        // Redirigir solo si el usuario está en la base de datos
+        header('Location: ../views/hamilton.php');
+        exit(); // Asegura que el script se detenga después de la redirección
+    } else {
+        // Si no hay filas, el usuario no está en la base de datos
+        echo "Usuario o contraseña incorrectos";
+        // Puedes agregar aquí algún código adicional, como un enlace de regreso al formulario de inicio de sesión
+    }
+
+    // Cerrar la conexión después de usarla
+    mysqli_close($conexion);
+?>
+
 
   
 
