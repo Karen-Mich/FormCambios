548fda6dcd4a23c52c9ea12e06b963d91ecf2739
Hamilton Jumbo <hamil.jumbo@gmail.com>
2023-12-03 14:30:53 -0500

    CambiosFinales

--- includes/_function.php
+++ includes/_function.php
@@ -4,26 +4,14 @@
     session_start();
     $_SESSION['nombre'] = $nombre;
     $conexion = mysqli_connect("localhost", "root", "admin", "formulario_de_cambios");
-    
     $consulta = "SELECT * FROM usuario WHERE NOM_USU='$nombre' AND CON_USU='$password'";
     $resultado = mysqli_query($conexion, $consulta);
+    $filas=mysqli_fetch_array($resultado);
     
-    // Verificar si hay filas en el resultado (usuario válido)
     if (mysqli_num_rows($resultado) > 0) {
-        // Redirigir solo si el usuario está en la base de datos
-        header('Location: http://localhost/FormCambios/views/hamilton.php');
-        exit(); // Asegura que el script se detenga después de la redirección
+        header('Location: ../views/hamilton.php');
+        exit(); 
     } else {
-        // Si no hay filas, el usuario no está en la base de datos
-        header('http://localhost/FormCambios/includes/login.php');
+        header('Location: login.php');
         session_destroy();
-        // Puedes agregar aquí algún código adicional, como un enlace de regreso al formulario de inicio de sesión
     }
-
-    // Cerrar la conexión después de usarla
-    mysqli_close($conexion);
-?>
-
-
-  
-
