941bb6b6385adb634469fd92414bc3727af9073d
Hamilton Jumbo <hamil.jumbo@gmail.com>
2023-12-03 11:53:10 -0500

    Edicion De funcion del Login

--- login/_function.php
+++ login/_function.php
@@ -1,10 +1,27 @@
 <?php
-    $nombre=$_POST['nombre'];
-    $password=$_POST['password'];
-    session_start();
-    $_SESSION['nombre']=$nombre;
-    $conexion=mysqli_connect("localhost","root","admin","formulario_de_cambios");
-    $consulta= "SELECT * FROM usuario WHERE NOM_USU='$nombre' AND CON_USU='$password'";
-    $resultado=mysqli_query($conexion, $consulta);
-    $filas=mysqli_fetch_array($resultado);
-        header('Location: ../views/edwin.php');
+session_start();
+
+$nombre = $_POST['nombre'];
+$password = $_POST['password'];
+
+$conexion = mysqli_connect("localhost", "root", "admin", "formulario_de_cambios");
+
+if (!$conexion) {
+    die("Error de conexión: " . mysqli_connect_error());
+}
+
+$consulta = "SELECT * FROM usuario WHERE NOM_USU = ? AND CON_USU = ?";
+$statement = mysqli_prepare($conexion, $consulta);
+mysqli_stmt_bind_param($statement, "ss", $nombre, $password);
+mysqli_stmt_execute($statement);
+$resultado = mysqli_stmt_get_result($statement);
+
+if ($filas = mysqli_fetch_array($resultado)) {
+    $_SESSION['nombre'] = $nombre;
+    header('Location: ../interfaz/hamilton.php');
+} else {
+    echo "Error en la autenticación";
+}
+
+mysqli_close($conexion);
+?>
