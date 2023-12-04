1a5bdf1d4feed0a402ecf7396e72e4f4af0bfdb4
Oscar Ramirez <ramirez7oscar8203@gmail.com>
2023-12-04 03:45:04 -0500

    Estilizado de el modal

--- peticiones.php
+++ peticiones.php
@@ -15,6 +15,7 @@ $id_peticion;
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Document</title>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <link rel="stylesheet" href="./peticiones.css">
 </head>
 <body>
 <div class="row g-0 text-center p-3">
@@ -84,6 +85,7 @@ $id_peticion;
           while ($fila = mysqli_fetch_assoc($resultado)) {
               ?>
               <!-- Fila con datos de la base de datos -->
+              
               <div class="row g-0 text-center p-3 clickable-row" data-id-cam="<?php echo $fila['ID_CAM']; ?>">
   <div class="col">
     <div class="card">
@@ -149,10 +151,9 @@ $id_peticion;
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable"">
+  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" style="max-width: 800px;">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body" id="respuestaModal">
